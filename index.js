280ff2073abb6e2c2ed7189ca4ffe5293949073d
MaenSZubaydi <maenzubaydi@gmail.com>
2022-05-24 10:36:42 +1000

    new

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const element = document.querySelector('main')
+element.remove();
+
+const elem= document.createElement('h1');
+elem.setAttribute("id", "victory");
+elem.innerHTML = "Maen is the champion";
+
+const newHeader = elem;
+
+
+
